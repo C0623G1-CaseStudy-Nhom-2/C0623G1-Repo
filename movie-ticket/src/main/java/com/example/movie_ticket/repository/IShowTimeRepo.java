b6f766278e1b177ab13a5f34b6fb3805ce1ff127
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-10-30 15:56:41 +0700

    delete

--- movie-ticket/src/main/java/com/example/movie_ticket/repository/IShowTimeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.movie_ticket.repository;
-
-import com.example.movie_ticket.model.ShowTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IShowTimeRepo extends JpaRepository<ShowTime, Long> {
-}
--- movie-ticket/src/main/java/com/example/movie_ticket/service/IShowTimeService.java
+++
@@ -1,4 +0,0 @@
-package com.example.movie_ticket.service;
-
-public interface IShowTimeService {
-}
